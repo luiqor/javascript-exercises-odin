0585ddc13bfed898635a47fc6921c264934ed03d
luiqor <vohnenna@gmail.com>
2024-04-11 12:18:20 +0300

    exercise 11 done

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (arrayObjects) {
+  return arrayObjects
+    .filter((element) => typeof element === "object")
+    .map((element) => element.title);
 };
-
 // Do not edit below this line
 module.exports = getTheTitles;
