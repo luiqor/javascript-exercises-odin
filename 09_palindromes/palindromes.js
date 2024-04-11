08aeb0d185dec98232b15cf773434d3ae0433a0d
luiqor <vohnenna@gmail.com>
2024-04-11 11:49:35 +0300

    exercose 9 done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (originalString) {
+    originalLowerCased = originalString.toLowerCase();
+    originalArrayFiltred = originalLowerCased.split("")
+                    .filter((char) => ![" ", ",", "!", "?", "."].includes(char));
+    palindromedString = [...originalArrayFiltred].reverse().join("");
 
+    return palindromedString.localeCompare(originalArrayFiltred.join("")) === 0;
 };
 
 // Do not edit below this line
