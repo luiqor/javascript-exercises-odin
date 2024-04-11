a11f1fa8e406e811f96cb1fa1baa3ed3e8c7ae6b
luiqor <vohnenna@gmail.com>
2024-04-11 11:29:36 +0300

    exercise 8 done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  let finalResult = 0;
+  for (let value of array){
+    finalResult += value;
+  }
+  return finalResult;
 };
 
-const multiply = function() {
+const multiply = function (array) {
+  if (array.length === 0) return 0;
 
+  let finalResult = 1;
+  for (let value of array){
+    finalResult *= value;
+  }
+  return finalResult;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  if (a === 0) return 1;
+
+  let finalResult = 1;
+  for (let value = a; value > 1; value--) {
+    finalResult *= value;
+  }
+  return finalResult;
 };
 
 // Do not edit below this line
