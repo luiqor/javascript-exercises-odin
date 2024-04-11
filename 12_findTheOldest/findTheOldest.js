63d43844dfae1abd53d8ce5a9ce5a62b9a2df760
luiqor <vohnenna@gmail.com>
2024-04-11 12:35:23 +0300

    exercise 12 done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (originalArray) {
+  return originalArray
+    .filter((element) => typeof element === "object")
+    .reduce((oldest, current) => {
+      const oldestAge = oldest.yearOfDeath
+        ? oldest.yearOfDeath - oldest.yearOfBirth
+        : new Date().getFullYear() - oldest.yearOfBirth;
+      const currentAge = current.yearOfDeath
+        ? current.yearOfDeath - current.yearOfBirth
+        : new Date().getFullYear() - current.yearOfBirth;
+      return oldestAge < currentAge ? current : oldest;
+    });
 };
 
 // Do not edit below this line
