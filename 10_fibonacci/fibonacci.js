88095b32b39cd90886b886be41e968543eac1c65
luiqor <vohnenna@gmail.com>
2024-04-11 12:04:34 +0300

    exercise 10 done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function (givenNumber) {
+    if (isNaN(givenNumber) || givenNumber < 0) return "OOPS";
+    let fibonnaciArray = [0, 1];
+    for (let i = 2; i <= givenNumber; i++) {
+    fibonnaciArray[i] = fibonnaciArray[i - 2] + fibonnaciArray[i - 1];
+    }
+    return fibonnaciArray[givenNumber];
 };
 
 // Do not edit below this line
