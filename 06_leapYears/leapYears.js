87a93db8c9170f43dc9cf52d8c8185390d0e1f9b
luiqor <vohnenna@gmail.com>
2024-04-11 11:10:02 +0300

    task 7 done

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(yearToExamine) {
+    if (
+      yearToExamine % 4 == 0 &&
+      yearToExamine % 100 != 0 ||
+      yearToExamine % 400 == 0
+    )
+      return true;
+    return false;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitTemp) {
+  return Math.round(((5 / 9) * (fahrenheitTemp - 32)) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemp) {
+  return Math.round((9 / 5 * celsiusTemp  + 32) * 10) / 10;
 };
 
 // Do not edit below this line
