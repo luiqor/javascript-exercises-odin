36fe31644ec520f9a948424790f9004d3642d2ce
luiqor <vohnenna@gmail.com>
2024-04-11 10:34:45 +0300

    tasks 02,03,04 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(originalString) {
+    return originalString.split("")
+                        .reverse()
+                        .join("");
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...unwantedElements) {
+  return array.filter((element) => !unwantedElements.includes(element));
 };
 
 // Do not edit below this line
